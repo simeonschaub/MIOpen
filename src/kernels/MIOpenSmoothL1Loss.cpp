0ebd01cd216e1e47f20f82ecd074fd01588cb091
long10024070 <long.luong@moreh.com.vn>
2024-05-02 18:08:10 +0000

    improved SmoothL1LossReduceForward performance

--- src/kernels/MIOpenSmoothL1Loss.cpp
+++ src/kernels/MIOpenSmoothL1Loss.cpp
@@ -141,25 +141,52 @@ extern "C" __global__ void SmoothL1LossReducedForward5d(const INPUT_TYPE* __rest
     smoothl1lossreducedforward5d<INPUT_TYPE, OUTPUT_TYPE>(I, T, lsum, beta, divisor, I_tv, T_tv);
 }
 
+__device__ FLOAT_ACCUM warp_reduce_sum(FLOAT_ACCUM val)
+{
+    if(warpSize >= 64)
+        val += __shfl_down(val, 32);
+    if(warpSize >= 32)
+        val += __shfl_down(val, 16);
+    if(warpSize >= 16)
+        val += __shfl_down(val, 8);
+    if(warpSize >= 8)
+        val += __shfl_down(val, 4);
+    if(warpSize >= 4)
+        val += __shfl_down(val, 2);
+    if(warpSize >= 2)
+        val += __shfl_down(val, 1);
+    return val;
+}
+
+__device__ FLOAT_ACCUM block_reduce_sum(FLOAT_ACCUM val)
+{
+    static __shared__ FLOAT_ACCUM shared[REDUCE_SIZE / warpSize];
+    auto lane = threadIdx.x % warpSize;
+    auto wid  = threadIdx.x / warpSize;
+
+    val = warp_reduce_sum(val);
+
+    if(lane == 0)
+        shared[wid] = val;
+    __syncthreads();
+
+    val = threadIdx.x < REDUCE_SIZE / warpSize ? shared[lane] : 0;
+    if(wid == 0)
+        val = warp_reduce_sum(val);
+
+    return val;
+}
+
 template <typename DTYPE>
 __device__ void losssum(const DTYPE* input, DTYPE* output, size_t N)
 {
-    static __shared__ FLOAT_ACCUM local_mem[REDUCE_SIZE];
     auto gid = blockIdx.x * blockDim.x + threadIdx.x;
-    auto lid = threadIdx.x;
 
-    local_mem[lid] = gid < N ? CVT_FLOAT2ACCUM(input[gid]) : static_cast<FLOAT_ACCUM>(0.0f);
-    __syncthreads();
+    FLOAT_ACCUM val = gid < N ? CVT_FLOAT2ACCUM(input[gid]) : static_cast<FLOAT_ACCUM>(0.0f);
+    val             = block_reduce_sum(val);
 
-    for(auto i = blockDim.x / 2; i > 0; i >>= 1)
-    {
-        if(lid < i)
-            local_mem[lid] += local_mem[lid + i];
-        __syncthreads();
-    }
-
-    if(lid == 0)
-        output[blockIdx.x] = CVT_ACCUM2FLOAT(local_mem[0]);
+    if(threadIdx.x == 0)
+        output[blockIdx.x] = CVT_ACCUM2FLOAT(val);
 }
 
 extern "C" __global__ void
