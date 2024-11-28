e78bd3824a698051c066f2155a8715f9a00ba84d
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2024-11-28 14:37:34 +0100

    Fix constexpr_type_name() for gcc and msvc

--- src/include/miopen/type_name.hpp
+++ src/include/miopen/type_name.hpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2017 Advanced Micro Devices, Inc.
+ * Copyright (c) 2017-2024 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,6 +29,9 @@
 
 #include <string>
 #include <string_view>
+#if defined(_MSC_VER) && !defined(__clang__) && !defined(__GNUC__)
+#include <type_traits>
+#endif
 
 #include <miopen/config.h> // For BUILD_SHARED_LIBS and MIOPEN_ENABLE_FIN_INTERFACE
 
@@ -38,14 +41,27 @@ template <class T>
 constexpr std::string_view constexpr_type_name()
 {
 #if defined(__clang__) || defined(__GNUC__)
+    // clang or gcc
     constexpr auto full_name = std::string_view{__PRETTY_FUNCTION__};
+#elif defined(_MSC_VER)
+    // msvc
+    constexpr auto full_name = std::string_view{__FUNCSIG__};
+#endif
+
+    // The substring with the data type name is located within the original string, between the prefix and the suffix, with the prefix always not at the beginning of the string and the suffix always at the end of the string.
 #if defined(__clang__)
+    // clang
     constexpr auto prefix = std::string_view{"[T = "};
-#else  // !clang
-    constexpr auto prefix = std::string_view{"[with T = "};
-#endif // !clang
     constexpr auto suffix = std::string_view{"]"};
-#endif // clang || gcc
+#elif defined(__GNUC__)
+    // gcc
+    constexpr auto prefix = std::string_view{"[with T = "};
+    constexpr auto suffix = std::string_view{"; std::string_view = std::basic_string_view<char>]"};
+#elif defined(_MSC_VER)
+    // msvc
+    constexpr auto prefix = std::string_view{"constexpr_type_name<"};
+    constexpr auto suffix = std::string_view{">(void)"};
+#endif
 
     constexpr auto prefix_pos = full_name.find(prefix);
     static_assert(prefix_pos != std::string_view::npos);
@@ -60,7 +76,30 @@ constexpr std::string_view constexpr_type_name()
 
     constexpr auto name = full_name.substr(pos, count);
 
+#if defined(__clang__) || defined(__GNUC__)
+    // clang or gcc
     return name;
+#elif defined(_MSC_VER)
+    // msvc
+    if constexpr(std::is_compound_v<T>)
+    {
+        // For compound data types, the string contains the keyword 'class/struct/union/enum' before the data type name, separated by a space.
+        constexpr auto sep = std::string_view{" "};
+        constexpr auto sep_pos = name.find(sep);
+        static_assert(sep_pos != std::string_view::npos);
+        static_assert(sep_pos != 0); // must not be at the 0 position
+
+        constexpr auto name_pos = sep_pos + sep.size();
+        constexpr auto type_name = name.substr(name_pos);
+        static_assert(type_name.size() > 0);
+
+        return type_name;
+    }
+    else
+    {
+        return name;
+    }
+#endif
 }
 
 template <class T>
