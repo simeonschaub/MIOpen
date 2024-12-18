21e068256a0d32b242ccf3c3b26227129137d2ae
Vsevolod Golovko <gvsevolo@amd.com>
2024-12-18 19:23:16 +0100

    WinoFury names uniqueness

--- test/gtest/unit_conv_solver_ConvWinoFuryRxS.cpp
+++ test/gtest/unit_conv_solver_ConvWinoFuryRxS.cpp
@@ -63,47 +63,52 @@ const auto& GetTestParams()
 
 } // namespace
 
-TEST_P(GPU_UnitTestConvSolverFwd_FP16, ConvWinoFuryRxSf2x3)
+using GPU_UnitTestConvSolverWinoFuryFwd_FP16 = GPU_UnitTestConvSolverFwd_FP16;
+using GPU_UnitTestConvSolverWinoFuryBwd_FP16 = GPU_UnitTestConvSolverBwd_FP16;
+using GPU_UnitTestConvSolverWinoFuryWrw_FP16 = GPU_UnitTestConvSolverWrw_FP16;
+using CPU_UnitTestConvSolverDevApplicabilityWinoFuryFwd_NONE = CPU_UnitTestConvSolverDevApplicabilityFwd_NONE;
+
+TEST_P(GPU_UnitTestConvSolverWinoFuryFwd_FP16, ConvWinoFuryRxSf2x3)
 {
     this->RunTest(miopen::solver::conv::ConvWinoFuryRxS<2, 3>{});
 };
 
-TEST_P(GPU_UnitTestConvSolverBwd_FP16, ConvWinoFuryRxSf2x3)
+TEST_P(GPU_UnitTestConvSolverWinoFuryBwd_FP16, ConvWinoFuryRxSf2x3)
 {
     this->RunTest(miopen::solver::conv::ConvWinoFuryRxS<2, 3>{});
 };
 
-TEST_P(GPU_UnitTestConvSolverWrw_FP16, ConvWinoFuryRxSf2x3)
+TEST_P(GPU_UnitTestConvSolverWinoFuryWrw_FP16, ConvWinoFuryRxSf2x3)
 {
     this->RunTest(miopen::solver::conv::ConvWinoFuryRxS<2, 3>{});
 };
 
-TEST_P(CPU_UnitTestConvSolverDevApplicabilityFwd_NONE, ConvWinoFuryRxSf2x3)
+TEST_P(CPU_UnitTestConvSolverDevApplicabilityWinoFuryFwd_NONE, ConvWinoFuryRxSf2x3)
 {
     this->RunTest(miopen::solver::conv::ConvWinoFuryRxS<2, 3>{});
 };
 
 // Smoke tests
 INSTANTIATE_TEST_SUITE_P(Smoke,
-                         GPU_UnitTestConvSolverFwd_FP16,
+                         GPU_UnitTestConvSolverWinoFuryFwd_FP16,
                          testing::Combine(testing::Values(GetTestParams()),
                                           testing::Values(miopenConvolutionAlgoWinograd),
                                           testing::ValuesIn(GetConvTestCases(miopenHalf))));
 
 INSTANTIATE_TEST_SUITE_P(Smoke,
-                         GPU_UnitTestConvSolverBwd_FP16,
+                         GPU_UnitTestConvSolverWinoFuryBwd_FP16,
                          testing::Combine(testing::Values(GetTestParams()),
                                           testing::Values(miopenConvolutionAlgoWinograd),
                                           testing::ValuesIn(GetConvTestCases(miopenHalf))));
 
 INSTANTIATE_TEST_SUITE_P(Smoke,
-                         GPU_UnitTestConvSolverWrw_FP16,
+                         GPU_UnitTestConvSolverWinoFuryWrw_FP16,
                          testing::Combine(testing::Values(GetTestParams()),
                                           testing::Values(miopenConvolutionAlgoWinograd),
                                           testing::ValuesIn(GetConvTestCasesWrw(miopenHalf))));
 
 // Device applicability test
 INSTANTIATE_TEST_SUITE_P(Smoke,
-                         CPU_UnitTestConvSolverDevApplicabilityFwd_NONE,
+                         CPU_UnitTestConvSolverDevApplicabilityWinoFuryFwd_NONE,
                          testing::Combine(testing::Values(GetTestParams()),
                                           testing::Values(GetConvTestCases(miopenHalf)[0])));
